b5d4e11bd17e39c796d6a8d870917267e48c1ae1
Hubert Yang <offspring7890@yahoo.com>
2019-10-03 14:34:11 -0700

    finish testing

---
+++ postgrator-config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+console.log(process.env.NODE_ENV)
+console.log(process.env.TEST_DB_URL)
+
+module.exports = {
+  "migrationDirectory": "migrations",
+  "driver": "pg",
+  // "database": (process.env.NODE_ENV === 'test') 
+  //   ? "blogful_test"
+  //   : "blogful"
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DB_URL
+    : process.env.DB_URL,
+}
+
---
+++ src/app.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const ArticlesService = require('./articles-service');
+
+const app = express();
+
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles.map(article => ({
+        id: article.id,
+        title: article.title,
+        style: article.style,
+        content: article.content,
+        date_published: new Date(article.date_published)
+      })))
+    })
+    .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      if(!article) {
+        return res.status(404).json({
+          error: { message: `Article doesn't exist` }
+        })
+      }
+      res.json(article)
+    })
+    .catch(next)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error);
+    response = { message: error.message, error}
+  }
+  res.status(500).json(response)
+})
+
+module.exports = app;
